92968018da03fd2257a5966a8f593c35f1ee62dd
eduardojigub <eduardobrunoit@gmail.com>
2023-04-07 18:30:05 -0300

    feat: adding styles on ScreenHeaderBtn

--- app/index.js
+++ app/index.js
@@ -23,7 +23,7 @@ const Home = () => {
             <ScreenHeaderBtn iconUrl={icons.menu} dimension={'60%'} />
           ),
           headerRight: () => (
-            <ScreenHeaderBtn iconUrl={icons.menu} dimension={'100%'} />
+            <ScreenHeaderBtn iconUrl={images.profile} dimension={'100%'} />
           ),
           headerTitle: '',
         }}
