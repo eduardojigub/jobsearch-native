309bb9108f7309c83abee66090dd71d4de427fd9
eduardojigub <eduardobrunoit@gmail.com>
2023-04-07 18:48:02 -0300

    feat: welcome component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -11,8 +11,12 @@ import { useRouter } from 'expo-router';
 import { icons, SIZES } from '../../../constants';
 import styles from './welcome.style';
 
+const jobTypes = ['Full-time', 'Part-time', 'Contractor'];
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-time');
+
   return (
     <View>
       <View styles={styles.container}>
@@ -21,8 +25,42 @@ const Welcome = () => {
       </View>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} />
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder={'What are you looking for?'}
+          />
         </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity
+                style={styles.tab(activeJobType, item)}
+                onPress={() => {
+                  setActiveJobType(item);
+
+                  router.push(`/search/${item}`);
+                }}
+              >
+                <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+              </TouchableOpacity>
+            );
+          }}
+        />
       </View>
     </View>
   );
