26dd95da0795b54b6eaa7a3d26b0512027125ac1
eduardojigub <eduardobrunoit@gmail.com>
2023-04-10 19:38:39 -0300

    feat: add fixes to useFetch hook

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -10,11 +10,17 @@ import { useRouter } from 'expo-router';
 import { COLORS, SIZES } from '../../../constants';
 import styles from './popularjobs.style';
 import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import useFetch from '../../../hook/useFetch';
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+
+  const { data, isLoading, error } = useFetch('search', {
+    query: 'React developer',
+    num_pages: 1,
+  });
+
+  console.log(data);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,53 +1,54 @@
 import { useEffect, useState } from 'react';
-import axios from axios;
-import { RAPID_API_KEY } from '@env'
+import axios from 'axios';
 
 const useFetch = (endpoint, query) => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      'X-RapidAPI-Key': 'e08dcf7bcfmsh421931c0dbea483p1d1557jsnb2ce87ab4190',
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
+    },
+    params: { ...query },
+  };
 
-    const rapidApikey  = RAPID_API_KEY
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 
-    const options = {
-        method: 'GET',
-        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-        headers: {
-            'X-RapidAPI-Key': rapidApikey,
-            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-          },
-        params: {... query},
-      };
-      
-      axios.request(options).then(function (response) {
-          console.log(response.data);
-      }).catch(function (error) {
-          console.error(error);
-      });
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert('There is an error');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-      const fetchData = async () => {
-        setIsLoading(true);
-        try{
-            const response = await axios.request(options);
-            setData(response.data.data);
-            setIsLoading(false)
-        } catch (error) {
-            setError(error)
-            alert('There is an error')
-        } finally {
-            setIsLoading(false)
-        }
-      }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-      useEffect(() => {
-        fetchData();
-      }, []);
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
 
-      const refetch = () => {
-        setIsLoading(true);
-        fetchData();
-      }
+  return { data, isLoading, error, refetch };
+};
 
-      return { data, isLoading, error, refetch }
-}
+export default useFetch;
